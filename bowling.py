f123e478f0834822089d3bb47a8a87c5456cfd60
Adam Godor <adam.godor@gmail.com>
2017-07-31 14:00:48 +0200

    get_value func organised into a dict

--- bowling.py
+++ bowling.py
@@ -31,15 +31,10 @@ def score(game):
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
-        return 10
-    elif char == '-':
-        return 0
+    signs = {'1': 1, '2': 2, '3': 3, '4': 4, \
+    '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, \
+    "X" : 10, "x" : 10, "/": 10, "-" : 0}
+    if char in signs.keys():
+        return signs.get(char)
     else:
         raise ValueError()
