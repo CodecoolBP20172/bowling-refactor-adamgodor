3a55ddbf170ba91c55d284f731dad7294a94e7b9
Adam Godor <adam.godor@gmail.com>
2017-08-01 11:31:48 +0200

    score func refractored

--- bowling.py
+++ bowling.py
@@ -3,22 +3,20 @@ def score(game):
     frame = 1
     in_first_half = True
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
-        else:
-            result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
         if frame < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
+                result += 10 - get_value(game[i-1])
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
+                result += 10 + get_value(game[i+1])
+                result += 10 - get_value(game[i])
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
-        last = get_value(game[i])
+        else:
+            result += get_value(game[i])
+
         if not in_first_half:
             frame += 1
         if in_first_half == True:
@@ -32,9 +30,7 @@ def score(game):
 
 def get_value(char):
     signs = {'1': 1, '2': 2, '3': 3, '4': 4, \
-    '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, \
-    "X" : 10, "x" : 10, "/": 10, "-" : 0}
+     '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, \
+     "X" : 10, "x" : 10, "/": 10, "-" : 0}
     if char in signs.keys():
         return signs.get(char)
-    else:
-        raise ValueError()
